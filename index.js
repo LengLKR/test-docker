b83058f0a4fa4e3a6842a530ce6e640753e69ed4
Leng <lengkrupanit555@gmail.com>
2025-05-13 17:33:19 +0700

    Using multiple services together with Docker Compose

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require('express')
+const mysql = require('mysql2/promise')
 const app = express()
 const port = 8000
+
+let conn = null
+
+const initMySQL = async () =>{
+    conn = await mysql.createConnection({
+        host: 'db',
+        user: 'root',
+        password: 'root',
+        database: 'tutorial'
+    })
+}
+
 
 app.get('/hello-world', (req, res) => {
     res.send('hello wold')
 })
 
-app.listen(port, () => {
+// path = GET /users สำหรับ get users ทั้งหมดที่บันทึกเข้าไปออกมา
+app.get('/user', async (req, res) => {
+    const [results] = await conn.query('SELECT * FROM users')
+    res.json(results)
+})
+
+
+app.listen(port, async() => {
+    await initMySQL()
     console.log(`Server running at http://localhost:${port}/`)
 })
